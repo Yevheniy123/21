3c8d4271755a609655d6dc49153fdc149721d6dc
Yevheniy123 <yevheniy.vinohradovv@gmail.com>
2022-05-06 21:39:11 +0300

    Initial commit

---
+++ hw23/src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import utils.DBUtils;
+import utils.Student;
+
+import java.sql.*;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+
+        DBUtils utils = new DBUtils();
+        Student student = new Student().setName("Yegen").setIdGroup(2).setComingYear(2003);
+//        utils.addStudent(student);
+        utils.deleteStudent(5);
+        utils.getALL().forEach(System.out::println);
+
+    }
+}
